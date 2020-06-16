7e3012060197817cbb52c722430a151d017634ad
Richard <evildea@gmail.com>
2020-06-16 13:52:08 +1000

    Extensive changes to subsystem

---
+++ VR-Trick-Shot/Assets/Scripts/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Vector3 m_startPosition;
+    private Rigidbody m_body;
+    private bool m_Reset = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_startPosition = transform.position;
+        m_body = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Reset()
+    {
+        transform.position = m_startPosition;
+        m_body.velocity = Vector3.zero;
+        m_body.useGravity = false;
+        m_Reset = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!m_Reset)
+        {
+            if (collision.collider.tag == "OutOfBounds")
+            {
+                m_Reset = true;
+                Invoke("Reset", 2f);
+            }
+        }
+    }
+
+}
---
+++ VR-Trick-Shot/Assets/Scripts/Handedness.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine.XR;
+using UnityEngine;
+
+
+public class Handedness : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InputDeviceCharacteristics leftTrackedControllerFilter = InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.TrackedDevice | InputDeviceCharacteristics.Left;
+        InputDeviceCharacteristics rightTrackedControllerFilter = InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.TrackedDevice | InputDeviceCharacteristics.Right;
+
+        List<InputDevice> foundLeftControllers = new List<InputDevice>();
+        List<InputDevice> foundRightControllers = new List<InputDevice>();
+
+        InputDevices.GetDevicesWithCharacteristics(leftTrackedControllerFilter, foundLeftControllers);
+        InputDevices.GetDevicesWithCharacteristics(rightTrackedControllerFilter, foundRightControllers);
+
+        var LeftController = GameObject.Find("Left Hand");
+        var RightController = GameObject.Find("Right Hand");
+
+        if (foundLeftControllers.Count == 0)
+            Destroy(LeftController);
+
+        if (foundRightControllers.Count == 0)
+            Destroy(RightController);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
