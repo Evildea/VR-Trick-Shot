48c38b747f42b21e58147f4c0bd0ccc6a807cc10
Richard <evildea@gmail.com>
2020-06-22 10:43:47 +1000

    Updated Score board

---
+++ VR-Trick-Shot/Assets/ScoreBoard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour
+{
+    private float m_GameTime;
+    public TextMesh Timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_GameTime += Time.deltaTime;
+        float TempTime = m_GameTime / 1000f;
+        string TempText = TempTime.ToString("#.00");
+        TempText = TempText.Replace(".", ":");
+        Timer.text = TempText;
+    }
+}
