ab84d373ced2ea24386a438c2bc74dfed2e68158
Richard <evildea@gmail.com>
2020-06-23 11:11:32 +1000

    Updated Physics

--- VR-Trick-Shot/Assets/Scripts/Ball.cs
+++ VR-Trick-Shot/Assets/Scripts/Ball.cs
@@ -4,26 +4,38 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    private Vector3 m_startPosition;
-    private Rigidbody m_body;
-    private bool m_Reset = false;
-    private GameManager m_ActiveGameManager;
+    private enum State { None, PullToCenter, SpinAndDrop }
+
+    private Vector3     m_OriginalPosition;
+    private Quaternion  m_OriginalRotation;
+    private Rigidbody   m_Ball                  = null;
+    private GameManager m_ActiveGameManager     = null;
+    private bool        m_Reset                 = false;
+    private State       m_BallState             = State.None;
+    private float       m_SnapToCenterHoopTimer = 0f;
+
+    private Collider m_HoopCollider;
+    private Vector3 m_PullToCenterOriginalLocation;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        m_startPosition = transform.position;
-        m_body = gameObject.GetComponent<Rigidbody>();
+        m_OriginalPosition  = transform.position;
+        m_OriginalRotation  = transform.rotation;
+        m_Ball              = gameObject.GetComponent<Rigidbody>();
         m_ActiveGameManager = FindObjectOfType<GameManager>();
     }
 
     private void Reset()
     {
-        transform.position = m_startPosition;
-        m_body.velocity = Vector3.zero;
-        m_body.angularVelocity = Vector3.zero;
-        m_body.useGravity = false;
-        m_Reset = false;
+        m_BallState             = State.None;
+        transform.position      = m_OriginalPosition;
+        transform.rotation      = m_OriginalRotation;
+        m_Ball.velocity         = Vector3.zero;
+        m_Ball.angularVelocity  = Vector3.zero;
+        m_Ball.useGravity       = false;
+        m_Reset                 = false;
         m_ActiveGameManager.ResetMultipler();
     }
 
@@ -33,8 +45,9 @@ public class Ball : MonoBehaviour
         {
             if (collision.collider.tag == "OutOfBounds")
             {
-                m_Reset = true;
+                m_Ball.useGravity = true;
                 Invoke("Reset", m_ActiveGameManager.RespawnDelay);
+                m_Reset = true;
             }
         }
     }
@@ -54,11 +67,37 @@ public class Ball : MonoBehaviour
                 Vector3 TargetPosition = other.transform.position;
                 TargetPosition.y += 0.15f;
                 Vector3 m_DirVector = (TargetPosition - transform.position).normalized;
-                m_body.AddForce(m_DirVector * .2f, ForceMode.VelocityChange);
+                m_Ball.AddForce(m_DirVector * .2f, ForceMode.VelocityChange);
 
+                m_BallState = State.PullToCenter;
+                m_SnapToCenterHoopTimer = 0f;
+                m_Ball.useGravity = false;
+                m_PullToCenterOriginalLocation = transform.position;
+                m_HoopCollider = other;
+                m_Ball.velocity = Vector3.zero;
+                m_Ball.angularVelocity = Vector3.zero;
+
+                m_ActiveGameManager.Score += 100 * m_ActiveGameManager.GetMultiplier();
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (m_BallState == State.PullToCenter)
+        {
+            m_SnapToCenterHoopTimer += Time.deltaTime;
+            if (m_SnapToCenterHoopTimer > 1f)
+                m_SnapToCenterHoopTimer = 1f;
+
+            transform.position = Vector3.Lerp(transform.position, m_HoopCollider.transform.position, m_SnapToCenterHoopTimer);
+
+            if (m_SnapToCenterHoopTimer == 1f)
+            {
+                m_BallState = State.None;
+                m_Ball.useGravity = true;
                 m_Reset = true;
                 Invoke("Reset", m_ActiveGameManager.RespawnDelay);
-                m_ActiveGameManager.Score += 100 * m_ActiveGameManager.GetMultiplier();
             }
         }
     }
